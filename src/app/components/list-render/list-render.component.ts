ebaeeb4ae4e7e52ecf2240ee255acecad1f33590
MarceloHcb <marcelohcb@msn.com>
2023-07-11 14:18:18 -0300

    tipagem

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -1,19 +1,23 @@
 import { Component } from '@angular/core';
-
+import { Animal } from 'src/app/Animals';
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
   styleUrls: ['./list-render.component.css']
 })
 export class ListRenderComponent {
-  animals = [
-    {name: 'Dog', sound: 'Woof!'},
-    {name: 'Cat', sound: 'Meow!'},
-    {name: 'Cow', sound: 'Moo!'},
-    {name: 'Fox', sound: 'Wa-pa-pa-pa-pa-pa-pow!'},
-    {name: 'Pig', sound: 'Oink!'},
-    {name: 'Duck', sound: 'Quack!'},
-    {name: 'Bird', sound: 'Tweet!'},
-    {name: 'Sheep', sound: 'Baa!'}
+  animals: Animal[] = [
+    {name: 'Dog', sound: 'Woof!', show: false},
+    {name: 'Cat', sound: 'Meow!', show: false},
+    {name: 'Cow', sound: 'Moo!', show: false},
+    {name: 'Fox', sound: 'Wa-pa-pa-pa-pa-pa-pow!', show: false},
+    {name: 'Pig', sound: 'Oink!', show: false},
+    {name: 'Duck', sound: 'Quack!', show: false},
+    {name: 'Bird', sound: 'Tweet!', show: false},
+    {name: 'Sheep', sound: 'Baa!', show: false},
   ]
+  
+  toggleSound(animal: Animal):void {
+    animal.show = !animal.show;
+  }
 }
